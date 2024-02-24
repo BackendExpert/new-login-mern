496aebfb1fcca15a3c05807bc2533c3f9fc7c35d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-25 05:12:44 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./LoginSignUp/Login";
 import SignUp from "./LoginSignUp/SignUp";
 
+ 
 function App() {
   return (
     <BrowserRouter>
--- client/src/LoginSignUp/Login.jsx
+++ client/src/LoginSignUp/Login.jsx
@@ -1,6 +1,33 @@
+import { useState } from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import axios from "axios"
+import { Link } from "react-router-dom"
+
 const Login = () => {
+    
   return (
-    <div>Login</div>
+    <div className="container">
+        <form>
+            <div className="">
+                <label htmlFor="">Email</label>
+                <input 
+                    type="email"
+                    className="form-control rounded-0"
+                    name="email"
+                    onChangeCapture={} 
+                    placeholder="Email"/>
+            </div>
+            <div className="">
+                <label htmlFor="">Password</label>
+                <input 
+                    type="password"
+                    className="form-control rounded-0"
+                    name="email"
+                    onChangeCapture={} 
+                    placeholder="Email"/>
+            </div>
+        </form>
+    </div>
   )
 }
 
