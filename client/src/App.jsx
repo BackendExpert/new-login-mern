a2b902693618c1eabeccd326c8a448e716d33cbd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-25 04:46:09 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./LoginSignUp/Login";
+import SignUp from "./LoginSignUp/SignUp";
+
 function App() {
   return (
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} ></Route>
+        <Route path="/SignUp" element={<SignUp />} ></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
