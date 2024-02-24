cffadd8d442dc7bda3546e2da0a08d635959fc03
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-25 04:59:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const bcrypy = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const cookieParser = require("cookie-parser")
+const UserModel = require("./Models/User")
 
 const app = express()
 app.use(express.json())
@@ -11,6 +12,15 @@ app.use(cors())
 app.use(cookieParser())
 
 mongoose.connect('mongodb://127.0.0.1:27017/employee')
+
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body;
+    bcrypy.hash(password, 10)
+    .then(hash => {
+        
+    })
+})
+
 
 app.listen(3001, () => {
     console.log("Server Running....!")
