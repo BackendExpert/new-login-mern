d73e3dea9a3b77e4f01cb5476f75a9a95503ad76
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-25 04:43:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/LoginSignUp/SignUp.jsx
+++ client/src/LoginSignUp/SignUp.jsx
@@ -1,7 +1,47 @@
+import { useState } from "react"
 
 const SignUp = () => {
+    const [name, SetName] = useState()
+    const [email, SetEmail] = useState()
+    const [password, SetPassword] = useState()
+
   return (
-    <div>SignUp</div>
+    <div>
+        <div className="container">
+            <form>
+                <div className="">
+                    <label htmlFor="">Name</label>
+                    <input 
+                        type="text"
+                        className="form-control rounded-0"
+                        name="uname"
+                        onChange={(e) => SetName(e.target.value)}
+                        />
+                </div>
+                <div className="">
+                    <label htmlFor="">Email</label>
+                    <input 
+                        type="email"
+                        className="form-control rounded-0"
+                        name="uname"
+                        onChange={(e) => SetEmail(e.target.value)}
+                        />
+                </div>
+                <div className="">
+                    <label htmlFor="">Password</label>
+                    <input 
+                        type="password"
+                        className="form-control rounded-0"
+                        name="uname"
+                        onChange={(e) => SetPassword(e.target.value)}
+                        />
+                </div>
+                <div className="">
+                    <button className="btn btn-success" type="submit">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
   )
 }
 
