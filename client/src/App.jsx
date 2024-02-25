1519cbbabd504f91392e33716b229b53c4062d64
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-25 05:51:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./LoginSignUp/Login";
 import SignUp from "./LoginSignUp/SignUp";
-
+import Dashboard from "./userNavigete/Dashboard"
+import HomePage from "./userNavigete/HomePage"
  
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} ></Route>
         <Route path="/SignUp" element={<SignUp />} ></Route>
+        <Route path="/Dashboard" element={<Dashboard />} ></Route>
+        <Route path="/Home" element={<HomePage />} ></Route>
       </Routes>
     </BrowserRouter>
   )
